7bba2f95426e775d64a594c95441395410185c6b
admin <admin@DESKTOP-7D86IBB>
2020-04-10 16:28:42 +0900

    .

--- hrms/src/main/java/com/miris/hrms/service/com/impl/Com0101ServiceImpl.java
+++ hrms/src/main/java/com/miris/hrms/service/com/impl/Com0101ServiceImpl.java
@@ -1,5 +1,7 @@
 package com.miris.hrms.service.com.impl;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
@@ -8,8 +10,10 @@ import com.miris.hrms.service.com.Com0101Service;
 import com.miris.hrms.service.com.mapper.Com0101Mapper;
 import com.miris.hrms.service.com.vo.ManagerVO;
 
-@Service
+@Service("com0101Service")
 public class Com0101ServiceImpl implements Com0101Service {
+	
+	public static Logger logger = LogManager.getLogger(Com0101ServiceImpl.class);
 
 	@Autowired
 	BCryptPasswordEncoder passwordEncoder;
@@ -19,12 +23,19 @@ public class Com0101ServiceImpl implements Com0101Service {
 	
 	@Override
 	public boolean userLoginIn(ManagerVO manager) throws Exception {
+		logger.debug("userLoginIn");
+		boolean result;
 		String encPasswd = passwordEncoder.encode(manager.getAdm_pw());
 		manager.setAdm_pw(encPasswd);
 		
-		ManagerVO rstVO = mapper.selectManagerInfo(manager);
+		ManagerVO resultVO = mapper.selectManagerInfo(manager);
+		if(resultVO != null) {
+			result = true;
+		} else {
+			result = false;
+		}
 
-		return false;
+		return result;
 	}
 
 }
